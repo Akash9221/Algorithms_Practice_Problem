96b9d9cde25ad32b01b94a7bd06f9ec44a05ed3e
“akashmane3938” <maneakash3938@gmail.com>
2022-12-11 22:22:41 -0800

    Binary Search

--- Algorithm_Practice_Problem/Algorithm_Practice_Problem/Program.cs
+++ Algorithm_Practice_Problem/Algorithm_Practice_Problem/Program.cs
@@ -7,6 +7,17 @@ namespace Algorithm_Practice_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("DataStructure Algorithm Problems");
+            BinarySearch bs = new BinarySearch();
+            string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+            Console.WriteLine("Please enter a string to be searched :");
+            string key = Console.ReadLine();
+            int result = bs.Binary_Search(arr, key);
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at " + "index " + result);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
