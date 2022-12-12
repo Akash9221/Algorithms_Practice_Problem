5df17679eb7faaa1859824016c6f61b12b307037
“akashmane3938” <maneakash3938@gmail.com>
2022-12-11 22:10:01 -0800

    DataStructure

---
+++ Algorithm_Practice_Problem/Algorithm_Practice_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DataStructure Algorithm Problems");
+        }
+    }
+}
