5778e54f2dfb0c2c84985c7d5b052ba31385eef5
“akashmane3938” <maneakash3938@gmail.com>
2022-12-11 22:40:28 -0800

    Sorting Using Bubble Sort

--- Algorithm_Practice_Problem/Algorithm_Practice_Problem/Program.cs
+++ Algorithm_Practice_Problem/Algorithm_Practice_Problem/Program.cs
@@ -6,17 +6,23 @@ namespace Algorithm_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DataStructure Algorithm Problems");
-            BinarySearch bs = new BinarySearch();
-            string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
-            Console.WriteLine("Please enter a string to be searched :");
-            string key = Console.ReadLine();
-            int result = bs.Binary_Search(arr, key);
-            if (result == -1)
-                Console.WriteLine("Element not present");
-            else
-                Console.WriteLine("Element found at " + "index " + result);
-            Console.WriteLine(result);
+            //AP1_BinarySearch
+            //Console.WriteLine("DataStructure Algorithm Problems");
+            //BinarySearch bs = new BinarySearch();
+            //string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+            //Console.WriteLine("Please enter a string to be searched :");
+            //string key = Console.ReadLine();
+            //int result = bs.Binary_Search(arr, key);
+            //if (result == -1)
+            //    Console.WriteLine("Element not present");
+            //else
+            //    Console.WriteLine("Element found at " + "index " + result);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //AP2_Bubble_Sort
+            Bubble_Sort bubbleSort = new Bubble_Sort();
+            bubbleSort.BubbleSort();
             Console.ReadLine();
         }
     }
